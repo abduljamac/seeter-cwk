a6acc5ae9ad749f6029c291d805c38be66d0ff6e
abduljamac <abduljama001@gmail.com>
2020-04-06 19:07:57 +0100

    merged master changes

--- src/sep/seeter/client/Client.java
+++ src/sep/seeter/client/Client.java
@@ -102,7 +102,11 @@ public class Client {
 
     // Run the client
     @SuppressFBWarnings(value = "DM_DEFAULT_ENCODING", justification = "When reading console, ignore default encoding warning")
+<<<<<<< HEAD
     public void runClient() throws IOException {
+=======
+    private void run() throws IOException {
+>>>>>>> more code cleanup
         BufferedReader reader = null;
         CLFormatter helper = null;
         try {
